508097d7d9f3c550811f67189ff7c98c4145bfdb
eimbens <e.imbens@gmail.com>
2024-03-13 13:06:06 +0100

    Add language picking menu to settings page

--- frontend/src/js/settings.js
+++ frontend/src/js/settings.js
@@ -1,3 +1,22 @@
+// import { updateLanguage } from "../../wailsjs/go/main/App";
+
+function changeLanguage() {
+  var language = document.getElementById("language-picker-select").value;
+  console.log("Language changed to " + language);
+
+  switch(language) {
+    case "de":
+      // updateLanguage(0);
+      break;
+    case "en-GB":
+      // updateLanguage(1);
+      break;
+    default:
+      // updateLanguage(1);
+  }
+
+}
+
 function openSettingsPage() {
     document.getElementById("page-contents").innerHTML = `
     <div class="setting" id="dark-mode">
@@ -8,22 +27,20 @@ function openSettingsPage() {
       </label>
     </div>
     <hr class="solid">
-    <div class="setting">
-      <span class="setting-description">Other setting</span>
-      <label class="switch">
-        <input type="checkbox">
-        <span class="slider round"></span>
-      </label>
-    </div>
-    <hr class="solid">
-    <div class="setting">
-      <span class="setting-description">Other setting</span>
-      <label class="switch">
-        <input type="checkbox">
-        <span class="slider round"></span>
-      </label>
-    </div>
+    <div class="setting" class="language-picker language-picker--flags js-language-picker" data-trigger-class="li4-btn li4-btn--subtle js-tab-focus">
+        <form action="" class="language-picker__form">
+          <label for="language-picker-select">Select your language</label>
+      
+          <select name="language-picker-select" id="language-picker-select">
+            <option lang="de" value="de">Deutsch</option>
+            <option lang="en" value="en-GB">English (GB)</option>
+          </select>
+          
+        </form>
+      </div>
     `;
+
+    document.getElementById("language-picker-select").addEventListener("change", () => changeLanguage());
 }
 
 document.getElementById("settings-button").addEventListener("click", () => openSettingsPage());
